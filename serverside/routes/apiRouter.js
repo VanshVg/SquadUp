67d1d56123abda0070be93e1885a3260b82f553b
Vansh Gandhi <vansh.vg18@gmail.com>
2023-08-24 23:11:35 +0530

    Setup task routing

--- serverside/routes/apiRouter.js
+++ serverside/routes/apiRouter.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const userRoutes = require("./userRoutes");
 const teamRoutes = require("./teamRoutes");
+const taskRoutes = require("./taskRoutes");
 
 const apiRouter = express.Router();
 
 apiRouter.use("/users", userRoutes);
 apiRouter.use("/teams", teamRoutes);
+apiRouter.use("/tasks", taskRoutes);
 
 module.exports = apiRouter;
