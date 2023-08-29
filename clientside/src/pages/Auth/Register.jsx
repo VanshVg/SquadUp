ce36d9b326166a7d0bd318b921fd172b4bae38c7
Vansh Gandhi <vansh.vg18@gmail.com>
2023-08-29 13:59:55 +0530

    Improved UI

--- clientside/src/pages/Auth/Register.jsx
+++ clientside/src/pages/Auth/Register.jsx
@@ -1,29 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import "./Auth.css";
 
 const Register = () => {
-  return (
-    <div className="register-container">
-      <form className="register-form">
-        <h1 className="register-heading" align="center">
-          Join Team Up
-        </h1>
-        <div className="input-group">
-          <input type="text" name="firstName" placeholder="First Name" required />
-          <input type="text" name="lastName" placeholder="Last Name" required />
-        </div>
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
-        <input type="text" name="username" placeholder="Username" required />
-        <input type="email" name="email" placeholder="Email" required />
-        <input type="password" name="password" placeholder="Create Password" required />
-        <input type="password" name="confirmPassword" placeholder="Confirm Password" required />
-        <button type="submit">Sign Up</button>
-      </form>
-      <p className="login-link">
-        Already Have An Account? <Link to="/auth/login">Login</Link>
-      </p>
-    </div>
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Team Up | Registration</title>
+      </Helmet>
+      <div className="register-container">
+        <form className="register-form">
+          <h1 className="register-heading" align="center">
+            Join Team Up
+          </h1>
+          <div className="input-group">
+            <input type="text" name="firstName" placeholder="First Name" required />
+            <input type="text" name="lastName" placeholder="Last Name" required />
+          </div>
+
+          <input type="text" name="username" placeholder="Username" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <div className="password-input">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              name="password"
+              placeholder="Create Password"
+              required
+            />
+            <FontAwesomeIcon
+              icon={passwordVisible ? faEye : faEyeSlash}
+              onClick={togglePasswordVisibility}
+              className="password-icon"
+            />
+          </div>
+          <div className="password-input">
+            <input
+              type={confirmPasswordVisible ? "text" : "password"}
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              required
+            />
+            <FontAwesomeIcon
+              icon={confirmPasswordVisible ? faEye : faEyeSlash}
+              onClick={toggleConfirmPasswordVisibility}
+              className="password-icon"
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+        <p className="login-link">
+          Already Have An Account? <Link to="/auth/login">Login</Link>
+        </p>
+      </div>
+    </>
   );
 };
 
