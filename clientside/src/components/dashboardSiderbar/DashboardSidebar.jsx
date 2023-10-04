5efaa64a957d08069e71183da58de9565cc925e0
vanshvg <vansh.vg18@gmail.com>
2023-10-04 10:20:18 +0530

    Updated sidebar

--- clientside/src/components/dashboardSiderbar/DashboardSidebar.jsx
+++ clientside/src/components/dashboardSiderbar/DashboardSidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import GroupsIcon from "@mui/icons-material/Groups";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import Tooltip from "@mui/material/Tooltip";
 import "./DashboardSidebar.css";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -31,31 +32,40 @@ const DashboardSidebar = () => {
         <div className="dashboard-sidebar" style={{ width: "180px" }}>
           <div
             className={`${
-              location.pathname === "/dashboard/home" ? "sidebar-item-active" : "sidebar-item"
+              location.pathname === "/dashboard/home"
+                ? "sidebar-item-active-open"
+                : "sidebar-item-open"
             }`}
             onClick={handleHome}
           >
-            <div className="sidebar-item-icon">
+            <div className="sidebar-item-icon-open">
               <HomeIcon />
             </div>
-            <div className="sidebar-item-text">Home</div>
+            <div className="sidebar-item-text-open">Home</div>
           </div>
-          <div
-            className={`${
-              location.pathname === "/dashboard/MyTeams" ? "sidebar-item-active" : "sidebar-item"
-            }`}
-            onClick={handleMyTeams}
-          >
-            <div className="sidebar-item-icon">
-              <GroupsIcon />
+          <div classname="sidebar-myteams-open">
+            <div
+              className={`${
+                location.pathname === "/dashboard/MyTeams"
+                  ? "sidebar-item-active-open"
+                  : "sidebar-item-open"
+              }`}
+              onClick={handleMyTeams}
+            >
+              <div className="myteams-arrow-open">
+                <ArrowRightIcon />
+              </div>
+              <div className="sidebar-myteams-icon-open">
+                <GroupsIcon />
+              </div>
+              <div className="sidebar-myteams-text-open">My Teams</div>
             </div>
-            <div className="sidebar-item-text">My Teams</div>
           </div>
-          <div className="sidebar-item" onClick={handleExit}>
-            <div className="sidebar-item-icon">
+          <div className="sidebar-item-open" onClick={handleExit}>
+            <div className="sidebar-item-icon-open">
               <ExitToAppIcon />
             </div>
-            <div className="sidebar-item-text">Exit Dashboard</div>
+            <div className="sidebar-item-text-open">Exit Dashboard</div>
           </div>
         </div>
       ) : (
@@ -63,30 +73,39 @@ const DashboardSidebar = () => {
           <Tooltip title="Dashboard Home">
             <div
               className={`${
-                location.pathname === "/dashboard/home" ? "sidebar-item-active" : "sidebar-item"
+                location.pathname === "/dashboard/home"
+                  ? "sidebar-item-active-close"
+                  : "sidebar-item-close"
               }`}
               onClick={handleHome}
             >
-              <div className="sidebar-item-icon">
+              <div className="sidebar-item-icon-close">
                 <HomeIcon />
               </div>
             </div>
           </Tooltip>
           <Tooltip title="My Teams">
-            <div
-              className={`${
-                location.pathname === "/dashboard/MyTeams" ? "sidebar-item-active" : "sidebar-item"
-              }`}
-              onClick={handleMyTeams}
-            >
-              <div className="sidebar-item-icon">
-                <GroupsIcon />
+            <div className="sidebar-myteams-close">
+              <div
+                className={`${
+                  location.pathname === "/dashboard/MyTeams"
+                    ? "sidebar-myteams-active-close"
+                    : "sidebar-myteams-close"
+                }`}
+                onClick={handleMyTeams}
+              >
+                <div className="myteams-arrow-open">
+                  <ArrowRightIcon />
+                </div>
+                <div className="sidebar-myteams-icon-close">
+                  <GroupsIcon />
+                </div>
               </div>
             </div>
           </Tooltip>
           <Tooltip title="Exit Dashboard">
-            <div className="sidebar-item" onClick={handleExit}>
-              <div className="sidebar-item-icon">
+            <div className="sidebar-item-close" onClick={handleExit}>
+              <div className="sidebar-item-icon-close">
                 <ExitToAppIcon />
               </div>
             </div>
