b96818868fe977b8ca7d070ea17e49d10e3f9b41
vanshvg <vansh.vg18@gmail.com>
2023-10-20 10:55:55 +0530

    Added Create Team page

--- clientside/src/pages/createTeam/CreateTeam.jsx
+++ clientside/src/pages/createTeam/CreateTeam.jsx
@@ -1,17 +1,34 @@
 import { React } from "react";
 import "./CreateTeam.css";
+import Helmet from "react-helmet";
 
 const CreateTeam = () => {
   return (
-    <div>
-      <div className="createteam-container">
-        <div className="createteam">
-          <div className="createteam-content">
-            <h1>This is Create Team page</h1>
+    <>
+      <Helmet>
+        <title>Create Team</title>
+      </Helmet>
+      <div className="createTeam-container">
+        <form className="createTeam-form">
+          <h1 className="createTeam-heading" align="center">
+            Create Team
+          </h1>
+
+          <div className="name-input">
+            <input type="text" name="name" placeholder="Enter Team name" required />
+            <input
+              type="text"
+              name="description"
+              placeholder="Enter description (Optional)"
+              required
+            />
           </div>
-        </div>
+          <button type="submit" className="loader-button">
+            Create Team
+          </button>
+        </form>
       </div>
-    </div>
+    </>
   );
 };
 
