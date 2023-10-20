daae4c01057e7d33c14e31528399d85cba3e8700
vanshvg <vansh.vg18@gmail.com>
2023-10-20 18:32:52 +0530

    Added Join Team functionality

--- clientside/src/pages/joinTeam/JoinTeam.jsx
+++ clientside/src/pages/joinTeam/JoinTeam.jsx
@@ -1,17 +1,113 @@
-import { React } from "react";
+import { React, useState } from "react";
+import Helmet from "react-helmet";
+import { ThreeDots } from "react-loader-spinner";
 import "./JoinTeam.css";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 const JoinTeam = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [joinTeamError, setJoinTeamError] = useState({});
+
+  const navigate = useNavigate();
+
+  const data = {
+    teamCode: "",
+  };
+
+  let userToken = Cookies.get("userToken");
+
+  const handleJoinTeam = (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/api/teams/joinTeam`, data, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      })
+      .then((resp) => {
+        if (resp.status === 200) {
+          navigate("/dashboard/home");
+        }
+      })
+      .catch((error) => {
+        const { status } = error.response;
+        if (status === 404) {
+          setJoinTeamError({
+            type: "not_found",
+            message: "Team with this team code not found",
+          });
+        } else if (status === 409) {
+          setJoinTeamError({
+            type: "conflict",
+            message: "You are already part of this team",
+          });
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const handleCodeChange = (e) => {
+    data.teamCode = e.target.value;
+  };
+
   return (
-    <div>
+    <>
+      <Helmet>
+        <title>Create Team</title>
+      </Helmet>
       <div className="jointeam-container">
-        <div className="jointeam">
-          <div className="jointeam-content">
-            <h1>This is Join Team page</h1>
+        <form className="jointeam-form" onSubmit={handleJoinTeam}>
+          <h1 className="jointeam-heading" align="center">
+            Join Team
+          </h1>
+
+          <div className="code-input">
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter Team Code"
+              required
+              onChange={handleCodeChange}
+            />
+            {joinTeamError ? (
+              <p
+                style={{
+                  color: "red",
+                  fontSize: "14px",
+                  marginTop: "1px",
+                  marginBottom: "5px",
+                  marginLeft: "1px",
+                }}
+              >
+                {joinTeamError.message}
+              </p>
+            ) : null}
           </div>
-        </div>
+          <button type="submit" className="loader-button">
+            {isLoading ? (
+              <div className="loader-container">
+                <ThreeDots
+                  type="ThreeDots"
+                  height={16}
+                  width={80}
+                  radius={9}
+                  color="white"
+                  ariaLabel="three-dots-loading"
+                  visible={true}
+                />
+              </div>
+            ) : (
+              "Join Team"
+            )}
+          </button>
+        </form>
       </div>
-    </div>
+    </>
   );
 };
 
